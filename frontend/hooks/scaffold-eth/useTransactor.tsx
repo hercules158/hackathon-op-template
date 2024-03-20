2dcc9fa461dce4a0d34642bdd7a2cbf7be8d68ff
Hércules Corsini <hercules158@users.noreply.github.com>
2024-03-19 21:45:49 -0300

    Correções

--- frontend/hooks/scaffold-eth/useTransactor.tsx
+++ frontend/hooks/scaffold-eth/useTransactor.tsx
@@ -133,6 +133,7 @@ export const useTransactor = (_signer?: Signer): TTransactionFunc => {
           }
         }
       }
+      
       console.error("⚡️ ~ file: useTransactor.ts ~ error", error);
     }
 
